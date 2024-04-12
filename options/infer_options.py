47a089c6df83adabd971bfda3b5ac4fae538bc2a
liuliqin <liuliqin@buaa.edu.cn>
2024-04-12 14:58:34 +0800

    HyperLDM 20240412

--- options/infer_options.py
+++ options/infer_options.py
@@ -10,7 +10,7 @@ class InferOptions(BaseOptions):
         parser.add_argument('--model_epoch', default='1801', help='Which model to test?')
         parser.add_argument('--test_flag', default='test_mini', help='train or test_mini')  # _mini
         parser.add_argument('--abun_flag', default=False, help='save abundance mat')
-        parser.add_argument('--gen_time', type=int, default=10, help='Which epoch model to latent')
+        parser.add_argument('--gen_time', type=int, default=10, help='how many times to run the generation model?')
         parser.add_argument('--latent_root', default='20230517_atten', help='which diffusion root')
         parser.add_argument('--latent_epoch', default='004000', help='Which epoch model to latent')
         parser.add_argument('--cal_performance', default=True, help='cal performance?')
